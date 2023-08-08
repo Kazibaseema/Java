c09076b5667fc92494e5ea57dc79d49dfd9c9521
Kazibaseema <120583274+Kazibaseema@users.noreply.github.com>
2023-08-08 22:48:25 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,8 @@
 class Sample{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+     System.out.println("Hello baseema");  
+        a=3;
+     System.out.println(a);  
     }  
 }  
