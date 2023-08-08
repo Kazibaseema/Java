10bb53349e88f5a0b0e51d5a734203598890be59
Kazibaseema <120583274+Kazibaseema@users.noreply.github.com>
2023-08-08 22:50:46 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,9 @@
 class Sample{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-     System.out.println("Hello baseema");  
-        a=3;
+     System.out.println("welcom to devops lab my students.");  
+        int a=3;
+        for(i=0;i<a;i++){
      System.out.println(a);  
-    }  
+    }  }
 }  
